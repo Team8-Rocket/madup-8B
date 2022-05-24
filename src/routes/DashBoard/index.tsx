2159ad68a2c25b239b85a1c025d1bf5d1f36ac90
hsw824 <ghdtjddn0824@naver.com>
2022-05-24 18:06:40 +0900

    Feat: add mediaStatus page
    Co-authored-by: Yeonghyeon Kim <kyhyun@users.noreply.github.com>

--- src/routes/DashBoard/index.tsx
+++ src/routes/DashBoard/index.tsx
@@ -1,7 +1,13 @@
 import styles from './dashboard.module.scss'
+import MediaStatus from './MediaStatus'
 
 const DashBoard = () => {
-  return <div className={styles.dashboard}>DashBoard</div>
+  return (
+    <div className={styles.dashboard}>
+      DashBoard
+      <MediaStatus />
+    </div>
+  )
 }
 
 export default DashBoard
